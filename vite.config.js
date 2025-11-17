59e420c3e3891dd5834b1622fe2968ed0a4cd921
kitchenverse688-spec <kitchenverse688@gmail.com>
2025-11-17 23:08:22 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AI-Kitchen-Equipment-Finder/' // <-- important for GitHub Pages
+  base: '/AI-Kitchen-Equipment-Finder/' // MUST match repo name
 });
